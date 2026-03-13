948df4ea689a3a8685f133fbdeba403d8f776a51
Centered101 <Centered101@outlook.com>
2026-03-13 15:12:43 +0700

    Update

--- config-students.js
+++ config-students.js
@@ -3,7 +3,7 @@ const API_URL = "https://script.google.com/macros/s/AKfycbycQG1QSp8be8zxgdZBn_11
 const LS_DATA = "asia_lb_session";
 const LS_TIME = "asia_lb_session_time";
 const SESSION_TTL = 8 * 60 * 60 * 1000;
-const API_SHOP = "https://script.google.com/macros/s/AKfycbwvxdCLL1RXjeMGxckdtBGZEIQHPwLAYftOQ6v2UCATe4Hg4JYff70VPLfjATmWfYGc/exec";
+const API_SHOP = "https://script.google.com/macros/s/AKfycbw8NE3zLb2g4npJX5bzH934n6iurjy1JduApSOmgL1m5ZSb3sbMAh8uc-XKuK0Qa4kE/exec";
 
 // ══════════════════════════════════════════════
 //  คำนวณระดับชั้น
